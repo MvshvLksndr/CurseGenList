c9376a4a371b23b9a4ccf57a631eb4320f25b674
stud <stud@107-4>
2023-04-14 16:58:53 +0400

    Добавьте файлы проекта.

---
+++ CurseGen.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListWPD
+{
+    public class CurseGen
+    {
+        Random random = new Random();
+        public string curse()
+        {
+            int rnd = random.Next(2, 4);
+            string result = string.Empty;
+
+            int one = random.Next(2);
+            if (one == 1)
+            {
+                for (int i = 0; i < rnd; i++)
+                {
+                    result += GetRandomGlas() + GetRandomSogl();
+                }
+                return result;
+            }
+            else
+            {
+                for (int i = 0; i < rnd; i++)
+                {
+                    result += GetRandomSogl() + GetRandomGlas();
+                }
+                return result;
+            }
+            
+        }
+
+        public string GetRandomSentence(int MaxWords)
+        {
+            string result = string.Empty;
+
+            for (int i = 0; i < random.Next(2,MaxWords); i++)
+            {
+                result += curse() + " ";
+            }
+            return result;
+        }
+
+        public string GetRandomGlas()
+        {
+            string glas = "аоуыэеёиюя";
+
+            return glas[random.Next(glas.Length)].ToString();
+        }
+
+        public string GetRandomSogl()
+        {
+            string sogl = "бвгджзйклмнпрстфхцчшщ";
+
+            return sogl[random.Next(sogl.Length)].ToString();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListWPD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public List<string> content = new List<string>();
+        public int Page = 0;
+        int MaxOnPage = 15;
+
+        CurseGen curse = new CurseGen();
+        public MainWindow()
+        {
+           
+            InitializeComponent();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                content.Add(curse.GetRandomSentence(10)); ;
+            }
+
+            int pages = GetMaxPages();
+            for (int i = 0; i < pages; i++)
+            {
+                Button button = new Button();
+                button.Padding = new Thickness(5);
+                button.Content = i +1;
+                button.Click += Button_Click;
+                button.Margin = new Thickness(5);
+
+                dpBtns.Children.Add(button);
+            }
+
+            UpdateList();
+
+
+
+        }
+        public void UpdateList()
+        {
+            lvContent.ItemsSource = content.Skip(Page * MaxOnPage).Take(MaxOnPage);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var b = (Button)sender;
+            Page = Convert.ToInt32(b.Content) - 1;
+            UpdateList();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (Page <= 0) return;
+            Page--;
+            UpdateList();
+        }
+
+        private void btnFrwrd_Click(object sender, RoutedEventArgs e)
+        {
+            if(Page >= GetMaxPages()-1) return;
+            Page++;
+            UpdateList();
+        }
+
+        public int GetMaxPages()
+        {
+            int max = 1+ (int)content.Count / MaxOnPage;
+
+            return max;
+        }
+    }
+}
